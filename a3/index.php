23dc566bef561e49f9cba3d600af4ee77633fbc4
ol222hf <ol222hf@student.lnu.se>
2020-10-15 00:44:30 +0200

    Testcode for a3

--- a3/index.php
+++ a3/index.php
@@ -1,1 +1,55 @@
 <?php
+
+session_start();
+
+require_once("model/RandomNumber.php");
+
+$minNumber = 1;
+$maxNumber = 100;
+$randValueSessionIndex = "randNumVal";
+$userGuess = "userGuess";
+$postGuess = "postGuess";
+$legendTitle = "Guess a number between " . $minNumber . "-" . $maxNumber . "";
+$message = "";
+
+$rn = new \Model\RandomNumber($minNumber, $maxNumber);
+
+if (!isset($_SESSION[$randValueSessionIndex])) {
+    $_SESSION[$randValueSessionIndex] = $rn->getValue();
+}
+
+echo '
+    <form method="post"> 
+    <fieldset>
+    <legend>' . $legendTitle . '</legend>
+    <p>' . $message . '</p>
+    
+    <label for="' . $userGuess . '">Username :</label>
+    <input type="text" id="' . $userGuess . '" name="' . $userGuess . '"/>
+    
+    <input type="submit" name="' . $postGuess . '" value="Login" />
+    </fieldset>
+    </form>
+    ';
+
+if (isset($_POST[$postGuess])) {
+    $guess = $_POST[$userGuess];
+    $randNumVal = $_SESSION[$randValueSessionIndex];
+
+    echo "You guessed: " . $guess . "<br/>";
+    echo "Number is: " . $randNumVal . "<br/>";
+
+    if (is_numeric($guess)) {
+        if (intval($guess) < $randNumVal) {
+            echo "Number is higher";
+        }
+        if (intval($guess) > $randNumVal) {
+            echo "Number is lower";
+        }
+        if (intval($guess) == $randNumVal) {
+            echo "Correct guess!";
+        }
+    } else {
+        echo "Only input a number";
+    }
+}
