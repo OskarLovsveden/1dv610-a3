d9f96913775c9b1bd3133eec0415184943e48a24
OskarLovsveden <ol222hf@student.lnu.se>
2020-10-15 12:46:25 +0200

    renaming and adding validation

--- a3/model/RandomNumber.php
+++ a3/model/RandomNumber.php
@@ -3,17 +3,35 @@
 namespace Model;
 
 class RandomNumber {
+    private $minValue;
+    private $maxValue;
     private $value;
 
     public function __construct(int $minValue, int $maxValue) {
+        if (!is_int($minValue)) {
+            throw new \Exception("minValue must be an integer");
+        }
+
+        if (!is_int($maxValue)) {
+            throw new \Exception("maxValue must be an integer");
+        }
+
         if ($minValue < 0) {
             throw new \Exception("minValue cannot be less than 0");
         }
 
+        $this->minValue = $minValue;
+        $this->maxValue = $maxValue;
         $this->value = rand($minValue, $maxValue);
     }
 
-    public function getValue() {
+    public function getValueToGuess() {
         return $this->value;
+    }
+    public function getMinValue() {
+        return $this->minValue;
+    }
+    public function getMaxValue() {
+        return $this->maxValue;
     }
 }
--- a3/view/GuessingGame.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace View;
-
-class GuessingGame {
-}
