346c43e437952be723b439168f82ce2a4345bca5
OskarLovsveden <ol222hf@student.lnu.se>
2020-10-16 14:04:43 +0200

    flash messages as its own class

---
+++ common/flash-message/FlashMessage.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('../common/session-storage/SessionStorage.php');
+
+class FlashMessage {
+    private static $flashMessageSessionIndex = __CLASS__ . '::SessionIndex';
+    private $flashMessageShouldPersistOneReload = false;
+
+    private $sessionStorage;
+
+    public function __construct() {
+        $this->sessionStorage = new \SessionStorage(self::$flashMessageSessionIndex);
+    }
+
+    public function set(string $message) {
+        $this->sessionStorage->store($message);
+        $this->flashMessageShouldPersistOneReload = true;
+    }
+
+    public function remove() {
+        if ($this->sessionStorage->hasValue()) {
+            $this->sessionStorage->removeValue();
+        }
+    }
+
+    public function get(): string {
+        if ($this->flashMessageShouldPersistOneReload) {
+            return $this->sessionStorage->getValue();
+        }
+
+        if ($this->sessionStorage->hasValue()) {
+            $message = $this->sessionStorage->getValue();
+            $this->sessionStorage->removeValue();
+
+            return $message;
+        }
+        return "";
+    }
+}
