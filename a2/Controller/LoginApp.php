8b600e08f319ff1337fb034e7cedb71ff763c258
OskarLovsveden <ol222hf@student.lnu.se>
2020-10-16 14:04:03 +0200

    renaming and refacturing

--- a2/Controller/LoginApp.php
+++ a2/Controller/LoginApp.php
@@ -2,6 +2,8 @@
 
 namespace Controller;
 
+require_once('../common/flash-message/FlashMessage.php');
+
 require_once('Controller/Login.php');
 require_once('Controller/Register.php');
 
@@ -10,22 +12,28 @@ require_once('view/Register.php');
 require_once('view/DateTime.php');
 require_once('view/Layout.php');
 
-class Application {
+class LoginApp {
     private $loginView;
     private $registerView;
     private $dateTimeView;
     private $layoutView;
+
     private $loginController;
     private $registerController;
+
+    private $flashMessage;
     private $userLoggedIn;
 
     public function __construct(\Authenticator $authenticator) {
+
         $this->loginView = new \View\Login($authenticator);
         $this->registerView = new \View\Register($authenticator);
         $this->dateTimeView = new \View\DateTime();
         $this->layoutView = new \View\Layout();
-        $this->loginController = new \Controller\Login($this->loginView, $authenticator);
-        $this->registerController = new \Controller\Register($this->registerView, $authenticator);
+        $this->flashMessage = new \FlashMessage();
+
+        $this->loginController = new \Controller\Login($authenticator, $this->loginView, $this->flashMessage);
+        $this->registerController = new \Controller\Register($this->registerView, $authenticator, $this->flashMessage);
     }
 
     public function run() {
--- a2/index.php
+++ a2/index.php
@@ -1,13 +1,12 @@
 <?php
-session_start();
 
 // Authenticator
 require_once('../common/authenticator/Authenticator.php');
 
 // App
-require_once('Controller/Application.php');
+require_once('Controller/LoginApp.php');
 
 $authenticator = new Authenticator();
 
-$app = new \Controller\Application($authenticator);
+$app = new \Controller\LoginApp($authenticator);
 $app->run();
