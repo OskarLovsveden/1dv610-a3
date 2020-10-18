5d025604e482edd95b042da8c777c02222c282a7
ol222hf <ol222hf@student.lnu.se>
2020-10-19 01:04:09 +0200

    adding gamestate

--- a3/Controller/Application.php
+++ a3/Controller/Application.php
@@ -2,6 +2,7 @@
 
 namespace A3\Controller;
 
+// Common
 require_once('../common/flash-message/FlashMessage.php');
 require_once('../common/session-storage/SessionStorage.php');
 
@@ -10,6 +11,7 @@ require_once("controller/Game.php");
 
 // Model
 require_once("model/RandomNumber.php");
+require_once("model/GameState.php");
 
 // View
 require_once("view/Layout.php");
@@ -17,7 +19,9 @@ require_once("view/Game.php");
 
 class Application {
     private $flashMessage;
+
     private $randomNumber;
+    private $gameState;
 
     private $gameView;
     private $layoutView;
@@ -26,16 +30,28 @@ class Application {
 
     public function __construct() {
         $this->flashMessage = new \FlashMessage();
-        $this->randomNumber = new \A3\Model\RandomNumber(1, 100);
 
-        $this->gameView = new \A3\View\Game($this->flashMessage, $this->randomNumber);
+        $this->randomNumber = new \A3\Model\RandomNumber(1, 100);
+        $this->gameState = new \A3\Model\GameState($this->randomNumber);
+
+
+
+        $this->gameView = new \A3\View\Game($this->flashMessage, $this->gameState);
         $this->layoutView = new \A3\View\Layout($this->gameView);
 
-        $this->gameController = new \A3\Controller\Game($this->flashMessage, $this->gameView, $this->randomNumber);
+        $this->gameController = new \A3\Controller\Game($this->flashMessage, $this->gameView, $this->gameState);
     }
 
     public function run() {
-        $this->gameController->doGuess();
-        $this->layoutView->renderHTML();
+        $gameWon = $this->gameState->isGameWon();
+
+        if ($gameWon) {
+            $this->gameController->doSaveHighScore();
+            $this->gameController->doResetGame();
+        } else {
+            $this->gameController->doPlay();
+        }
+
+        $this->layoutView->renderHTML($gameWon);
     }
 }
