d71a5f4e46512be901fe755bda44dac267277e59
ol222hf <ol222hf@student.lnu.se>
2020-10-18 20:59:09 +0200

    refactor with better naming + new A3 namespace

--- a3/view/Game.php
+++ a3/view/Game.php
@@ -1,42 +1,49 @@
 <?php
 
-namespace View;
+namespace A3\View;
 
 class Game {
-    private static $userGuess = "userGuess";
-    private static $postGuess = "postGuess";
+    private static $number = __CLASS__ . "number";
+    private static $guess = __CLASS__ . "guess";
 
-    private $randomNumber;
     private $title;
-    private $message = "";
+    private $message;
 
-    public function __construct(\Model\RandomNumber $randomNumber) {
-        $this->randomNumber = $randomNumber;
-        $this->title = "Guess a number between " . $this->randomNumber->getMinValue() . "-" . $this->randomNumber->getMaxValue();
+    private $flashMessage;
+
+    public function __construct(\FlashMessage $flashMessage, \A3\Model\RandomNumber $randomNumber) {
+        $this->title = "Guess a number between " . $randomNumber->getMinValue() . "-" . $randomNumber->getMaxValue();
+
+        $this->flashMessage = $flashMessage;
     }
 
     public function userWantsToGuess(): bool {
-        return isset($_POST[self::$postGuess]);
+        return isset($_POST[self::$number]);
     }
 
-    public function getUserGuess(): string {
-        if (!$_POST[self::$userGuess]) {
-            throw new \Exception("Please input a guess");
+    public function validateGuessForm() {
+        if (!$_POST[self::$number]) {
+            throw new \Exception("Please enter a guess to play");
         }
-        return $_POST[self::$userGuess];
+    }
+
+    public function getGuess(): string {
+        return $_POST[self::$number];
     }
 
     public function getHTML(): string {
+        $this->message = $this->flashMessage->get();
+
         return '
         <form method="post"> 
         <fieldset>
         <legend>' . $this->title . '</legend>
         <p>' . $this->message . '</p>
         
-        <label for="' . self::$userGuess . '">Write your guess here :</label>
-        <input type="text" id="' . self::$userGuess . '" name="' . self::$userGuess . '"/>
+        <label for="' . self::$number . '">Write your guess here :</label>
+        <input type="text" id="' . self::$number . '" name="' . self::$number . '"/>
         
-        <input type="submit" name="' . self::$postGuess . '" value="Guess" />
+        <input type="submit" name="' . self::$guess . '" value="Guess" />
         </fieldset>
         </form>
         ';
