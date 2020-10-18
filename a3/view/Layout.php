c82bc8e5b24cdd589d488d30248a03fc46d1a669
ol222hf <ol222hf@student.lnu.se>
2020-10-18 20:43:37 +0200

    layout view functional for gathering views

--- a3/view/Layout.php
+++ a3/view/Layout.php
@@ -3,4 +3,29 @@
 namespace View;
 
 class Layout {
+
+    private $gameView;
+
+    public function __construct(\View\Game $gameView) {
+        $this->gameView = $gameView;
+    }
+
+    public function renderHTML() {
+        $renderHTML = '
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <title>Login Example</title>
+        </head>
+        <body>
+        <h1>Assignment 2</h1>
+        <div class="container">
+        ' . $this->gameView->getHTML() . '
+        </div>
+        </body>
+        </html>';
+
+        echo $renderHTML;
+    }
 }
