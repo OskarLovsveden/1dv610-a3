ca2358ed1c932112b68e8c9a4fda6ebda04a09bb
ol222hf <ol222hf@student.lnu.se>
2020-10-18 20:45:05 +0200

    call on views to render html & impl. flashmsg

--- a3/Controller/GameApp.php
+++ a3/Controller/GameApp.php
@@ -2,27 +2,36 @@
 
 namespace Controller;
 
+require_once('../common/flash-message/FlashMessage.php');
+require_once('../common/session-storage/SessionStorage.php');
+
+// Controller
 require_once("Controller/Game.php");
+
+// Model
 require_once("model/RandomNumber.php");
+
+// View
+require_once("view/Layout.php");
 require_once("view/Game.php");
 
 class GameApp {
-    private $authenticator;
+    private $flashMessage;
 
-    public function __construct(\Authenticator $authenticator) {
-        $this->authenticator = $authenticator;
+    public function __construct() {
+        $this->flashMessage = new \FlashMessage();
     }
 
     public function run() {
 
         $rn = new \Model\RandomNumber(1, 100);
 
-        $gv = new \View\Game($rn);
-        $gameHTML = $gv->getHTML();
+        $gv = new \View\Game($this->flashMessage, $rn);
+        $lv = new \View\Layout($gv);
 
-        $gc = new \Controller\Game($this->authenticator, $gv, $rn);
+        $gc = new \Controller\Game($this->flashMessage, $gv, $rn);
         $gc->doGuess();
 
-        echo $gameHTML;
+        $lv->renderHTML();
     }
 }
