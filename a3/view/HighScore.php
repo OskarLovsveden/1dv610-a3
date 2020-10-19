f608093eb9d92ebad5b321b1c4bb38dde4f896d5
ol222hf <ol222hf@student.lnu.se>
2020-10-19 03:09:57 +0200

    getHTML loops over highscorelist

--- a3/view/HighScore.php
+++ a3/view/HighScore.php
@@ -3,8 +3,24 @@
 namespace A3\View;
 
 class HighScore {
+    private $highScore;
+
+    public function __construct(\A3\Model\HighScore $highScore) {
+        $this->highScore = $highScore;
+    }
 
     public function getHTML(): string {
-        return "";
+        $this->highScore->sort();
+        $hsi = $this->highScore->get();
+
+        $ret = "<ul>";
+        foreach ($hsi as $item) {
+            $ret .= "<li>";
+            $ret .= $item->getPlayer() . " | " . $item->getDifficulty() . " | " . $item->getScore();
+            $ret .= "</li>";
+        }
+        $ret .= "</ul>";
+
+        return $ret;
     }
 }
