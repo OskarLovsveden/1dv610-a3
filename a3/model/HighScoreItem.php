80b6f65ef4efd595ac659a785bd2bc1fff9b9b97
ol222hf <ol222hf@student.lnu.se>
2020-10-19 02:28:35 +0200

    highscore item class added

--- a3/model/HighScoreItem.php
+++ a3/model/HighScoreItem.php
@@ -3,4 +3,25 @@
 namespace A3\Model;
 
 class HighScoreItem {
+    private $player;
+    private $difficulty;
+    private $score;
+
+    public function __construct(string $player, int $difficulty, int $score) {
+        $this->player = $player;
+        $this->difficulty = $difficulty;
+        $this->score = $score;
+    }
+
+    public function getPlayer(): string {
+        return $this->player;
+    }
+
+    public function getDifficulty(): int {
+        return $this->difficulty;
+    }
+
+    public function getScore(): int {
+        return $this->score;
+    }
 }
