2378cc63a6b4867b1f326a364293e5421bdca478
ol222hf <ol222hf@student.lnu.se>
2020-10-19 02:28:23 +0200

    highscore class aded with get and add

--- a3/model/HighScore.php
+++ a3/model/HighScore.php
@@ -3,4 +3,13 @@
 namespace A3\Model;
 
 class HighScore {
+    private $items = array();
+
+    public function add(\A3\Model\HighScoreItem $toBeAdded) {
+        $this->items[] = $toBeAdded;
+    }
+
+    public function get(): array {
+        return $this->items;
+    }
 }
